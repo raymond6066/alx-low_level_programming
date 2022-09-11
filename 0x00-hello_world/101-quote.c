dcb7b7442d760e30127ee6d7290e5418572b5b2a
raymond6066 <adedipe6@gmail.com>
2022-09-11 04:23:28 -0700

    final changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
-			", 59);
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ch, sizeof(ch));
+
 	return (1);
 }
