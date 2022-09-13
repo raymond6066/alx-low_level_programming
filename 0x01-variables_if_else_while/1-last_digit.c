5cbc5304fbc6e2e5257833514e744fe00fb46c47
raymond6066 <adedipe6@gmail.com>
2022-09-12 18:48:43 -0700

    latest changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,4 +25,5 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
+}
 
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%1', num);
+		printf("%i", num);
 
 	printf("\n");
 
