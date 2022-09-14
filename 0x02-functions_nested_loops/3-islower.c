5142b6a5c1c5be34351aca8e92ac1d6c151119aa
raymond6066 <adedipe6@gmail.com>
2022-09-14 15:48:42 -0700

    g

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,20 @@
 /**
  * _islower - Entry point
  * Description: check if a character is lower
- * Return: 1if true. 0 if false
+ * @c: the integer value it recieves
+ * Return: 1 if true. 0 if false
  */
 int _islower(int c)
 {
-	int i = 'a';
+int i = 'a';
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		/* refer int c*/
-		if (c == 1)
-		{
-			return (1);
-		}
-	}
-	return (0);
+for (i = 'a'; i <= 'z'; i++)
+{
+/* refer int c*/
+if (c == 1)
+{
+return (1);
+}
+}
+return (0);
 }
