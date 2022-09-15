fed7be95a6560e142c52399273f43da5bf3472a2
raymond6066 <adedipe6@gmail.com>
2022-09-14 20:26:24 -0700

    h

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,6 @@ int main(void)
 {
 	int count = 2;
 	long int i = 1, j = 2;
-	long int k;
 
 	printf("%lu, ", i);
 	while (count <= 50)
@@ -23,7 +22,6 @@ int main(void)
 			printf("%lu, ", j);
 		}
 
-		k = j;
 		j += i;
 		count++;
 	}
