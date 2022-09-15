de1aa671e5b77665ac43113252c280b12a478e84
raymond6066 <adedipe6@gmail.com>
2022-09-14 18:00:50 -0700

    g

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 {
 	int row, column, product, tens, ones;
 
-	for (roe = 0; column <= 9; row++)
+	for (row = 0; column <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
