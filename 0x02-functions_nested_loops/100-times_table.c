6601d34653825ebed2892fc839d28abf1fb617fa
raymond6066 <adedipe6@gmail.com>
2022-09-14 19:56:05 -0700

    h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,12 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ *
+ * Description: prints the times table
+ *
+ * Return:void
  */
+
 void print_times_table(int n)
 {
 	int i, j, k;
