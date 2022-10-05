9946cfd283f5e017fcd0d5164e6c039a7875bbc4
raymond6066 <adedipe6@gmail.com>
2022-10-05 15:51:22 -0700

    back

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
